db7e54bebb6c9e1a9ffc72fcc816640e9387d615
Nut_NTCH <49915308+Nuttanich01@users.noreply.github.com>
2019-06-06 20:35:12 +0700

    Add files via upload

---
+++ Exercise4_Nuttanich_W.py
@@ -0,0 +1,9 @@
+foun_eng = 90
+gen_busin = 89
+intro_com = 87
+com_programming = 98.9
+print("-----Your Score-----")
+print("Foundation English :",foun_eng)
+print("General Business :",gen_busin)
+print("Introduction to Computer Systems :",intro_com)
+print("Computer Programming :",com_programming)
